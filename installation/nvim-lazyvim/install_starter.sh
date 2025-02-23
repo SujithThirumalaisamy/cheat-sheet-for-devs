5c63cee8397902aff29e5d54d000d9ca19bcd041
SujithThirumalaisamy <sujithmasi1267@gmail.com>
2025-02-23 15:51:53 +0530

    Updated lazyvim

--- installation/nvim-lazyvim/install_starter.sh
+++ installation/nvim-lazyvim/install_starter.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Download and install the latest version of Neovim
+curl -LO https:/github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo mkdir -p /opt/nvim
+sudo tar -xzf nvim-linux-x86_64.tar.gz --strip-components=1 -C /opt/nvim
+
+# Download other dependencies
+sudo apt update
+sudo apt install -y git curl build-essential ripgrep fd-find fzf
+
 # Clone the starter
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 # Remove the .git folder, so you can add it to your own repo later
 rm -rf ~/.config/nvim/.git
+
